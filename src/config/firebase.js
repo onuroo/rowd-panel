9410d9895daf94e87167bd9747fa363dd1382b86
onuroo <onurorhan04@gmail.com>
2017-08-01 22:35:26 +0300

    first init

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyBq39E1pWRb9pU00bt57O2mFDQ39R19wWY",
+  authDomain: "dyegame-90689.firebaseapp.com",
+  databaseURL: "https://dyegame-90689.firebaseio.com",
+  projectId: "dyegame-90689",
+  storageBucket: "dyegame-90689.appspot.com",
+  messagingSenderId: "996624568034"
+};
+export default {
+  config
+}
